db72d1aa2bfb8b1be1cb0785f3bead64e3006d78
alvesthw <109221000+alvesthw@users.noreply.github.com>
2024-11-02 13:10:33 -0300

    mudanças

--- consultas/public/javascript/jsPaginaCadastro/cadastro.js
+++ consultas/public/javascript/jsPaginaCadastro/cadastro.js
@@ -1,15 +1,24 @@
-//função pra tratamento de erro
-function confirmarLogin() {
+// Função para tratamento de erro no cadastro
+function confirmarCadastro() {
+    const name = document.getElementById('name');
+    const nameError = document.getElementById('nameError');
     const email = document.getElementById('email');
     const emailError = document.getElementById('emailError');
     const password = document.getElementById('password');
     const passwordError = document.getElementById('passwordError');
 
     // Reseta as mensagens de erro
+    nameError.classList.add('d-none');
     emailError.classList.add('d-none');
     passwordError.classList.add('d-none');
 
     let valid = true;
+
+    // Validação do nome
+    if (name.value.trim() === "") {
+        nameError.classList.remove('d-none'); // Mostra a mensagem de erro
+        valid = false;
+    }
 
     // Validação do email
     if (!email.validity.valid) {
@@ -25,9 +34,10 @@ function confirmarLogin() {
 
     // Se o formulário for válido, você pode enviar o formulário ou executar outra ação
     if (valid) {
-        console.log('Formulário válido! Enviando...');
+        console.log('Formulário de cadastro válido! Enviando...');
         return true; // Permite o envio do formulário
     } else {
         return false; // Impede o envio do formulário
     }
 }
+
